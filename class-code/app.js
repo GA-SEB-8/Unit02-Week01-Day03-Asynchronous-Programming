6fa0e6ad492ab4e273b743566be746ef5aeb57ef
omarakamal <omaraakamal@gmail.com>
2025-07-22 10:03:29 +0300

    update

--- class-code/app.js
+++ class-code/app.js
@@ -1,1 +1,5 @@
-console.log("Works")
+const axios = require("axios")
+// if we want to make requests to outside applications we need to use this package:
+// axios
+
+axios.get("https://omar-ga-class.onrender.com/students")
