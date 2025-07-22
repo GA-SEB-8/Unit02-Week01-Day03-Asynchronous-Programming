afce6380e11ea6046c866c8691db174f85eba558
omarakamal <omaraakamal@gmail.com>
2025-07-22 16:53:14 +0300

    update

--- class-code/mongoose-lesson/app.js
+++ class-code/mongoose-lesson/app.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose")
+const Book = require("./models/Book")
 
 async function connectToDB(){
 
@@ -17,31 +18,6 @@ async function connectToDB(){
 connectToDB()
 
 
-
-// creating the schema and model
-
-const bookSchema = new mongoose.Schema({
-    title: {
-        type:String,
-        required:true,
-        unique:true
-    },
-    author: String,
-    genres: [String], //array of strings
-    price: Number,
-    releaseYear: Number,
-    isBestSeller: {
-        type:Boolean,
-        default:false
-    },
-    rating:{
-        type:Number,
-        enum:[1,2,3,4,5]
-    }
-})
-
-// model
-const Book = mongoose.model("Book",bookSchema)
 
 
 async function createBook(){
