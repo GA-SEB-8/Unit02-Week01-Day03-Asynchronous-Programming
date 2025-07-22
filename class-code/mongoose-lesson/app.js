2ac5af3daa56d8580d41aedc5f400d72be2b59bd
omarakamal <omaraakamal@gmail.com>
2025-07-22 15:35:23 +0300

    update

--- class-code/mongoose-lesson/app.js
+++ class-code/mongoose-lesson/app.js
@@ -1,2 +1,17 @@
 const mongoose = require("mongoose")
 
+async function connectToDB(){
+
+    try{
+        await mongoose.connect("mongodb+srv://omar1:1234@cluster0.g3jfckx.mongodb.net/library?retryWrites=true&w=majority&appName=Cluster0")
+
+        console.log("Connected to Database")
+    }
+    catch(error){
+        console.log("Error: Issue connecting to Database")
+    }
+
+    
+}
+
+connectToDB()
