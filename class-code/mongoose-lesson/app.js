5cd4b9c4e2ac985bc7b3e3be302460d67fea2b41
omarakamal <omaraakamal@gmail.com>
2025-07-22 16:01:02 +0300

    update

--- class-code/mongoose-lesson/app.js
+++ class-code/mongoose-lesson/app.js
@@ -15,3 +15,48 @@ async function connectToDB(){
 }
 
 connectToDB()
+
+
+
+// creating the schema and model
+
+const bookSchema = new mongoose.Schema({
+    title: String,
+    author: String,
+    genres: [String], //array of strings
+    price: Number,
+    releaseYear: Number,
+    isBestSeller: Boolean
+})
+
+// model
+const Book = mongoose.model("Book",bookSchema)
+
+
+async function createBook(){
+
+    // we use the model for our CRUD
+    // .create makes a new book for me
+    const newBook = {
+        title: "Harry Potter 2",
+        author: "JK Rowling",
+        genres: ["adventure","Sci-Fi"],
+        price: 10,
+        releaseYear: 2001,
+        isBestSeller:true
+    }
+
+    const createdBook = await Book.create(newBook)
+    console.log(createdBook)
+}
+
+// createBook()
+
+
+async function getAllBooks(){
+// find(): returns all the books in the collections
+    const allBooks = await Book.find({releaseYear: 1999})
+    console.log(allBooks)
+}
+
+getAllBooks()
